8a77ae9378e830c06c8cd63a135e45c2ad5363b7
Samuel <ricardosalcedo72308@gmail.com>
2026-07-27 09:15:54 -0500

    Se agregó la opción crear cuenta bancaria al menú

--- funciones.py
+++ funciones.py
@@ -9,22 +9,26 @@ def menu_principal():
         print("==============================")
         print("1. Registrar cliente")
         print("2. Iniciar sesión")
-        print("3. Salir")
+        print("3. Crear cuenta bancaria")
+        print("4. Salir")
 
         opcion = input("\nSeleccione una opción: ")
 
         if opcion == "1":
 
-            print("\nFunción en desarrollo.")
+            print("\nFunción Registrar Cliente en desarrollo.")
 
         elif opcion == "2":
 
-            print("\nFunción en desarrollo.")
+            print("\nFunción Iniciar Sesión en desarrollo.")
 
         elif opcion == "3":
 
-            print("\nGracias por utilizar el sistema.")
+            print("\nFunción Crear Cuenta Bancaria en desarrollo.")
 
+        elif opcion == "4":
+
+            print("\nGracias por utilizar el sistema.")
             break
 
         else:
